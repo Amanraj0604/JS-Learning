f6bbc109ed3d8f4fe3454b0c8411c7e53e279e48
Aman Raj <117666238+Amanraj0604@users.noreply.github.com>
2024-08-08 12:13:40 +0000

    add updated code

---
+++ Basics/datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treate all JS code as newer version
+
+//alert("AmanRAj"); we are using nodeJS not a broweser 
+
+//premitivs Datatype
+
+// number => range = 2 to Power 53
+// bigint used for big number
+// string=>""
+// boolean => true/false
+// null =>it is a datatype as well as it's a standalone value
+// undefined => variable is declered but type is undefined
+// symbol => use for unique representation
+
+//object
+
+console.log(typeof "aman");//string
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
---
+++ Basics/type_conversion.js
@@ -0,0 +1,29 @@
+let id=787;
+let empId=String(id)
+// console.log(typeof(empId));
+
+// let eId=""//Type is number but value is 0
+// let eId="44jh"// Type is number but value is NaN
+// let eId=null //Type is number but value is 0
+// let eId=undefined //Type is number but value is NaN
+let eId=true //Type =number and value=1
+let eID=Number(eId);
+console.log(typeof(eID));
+console.log(eID);
+
+
+let num=1 // true 
+
+let num1=Boolean(num)
+console.log(num1);
+
+// let isAman=""// false
+// let isAman="aman" //true
+
+let isAman="278"
+
+let istrue=Boolean(isAman)
+
+console.table([istrue,typeof istrue])
+
+
