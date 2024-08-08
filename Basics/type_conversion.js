e6515d47f70520628f5559954dc571ac4abd37d4
Aman Raj <117666238+Amanraj0604@users.noreply.github.com>
2024-08-08 12:41:12 +0000

    update code

--- Basics/type_conversion.js
+++ Basics/type_conversion.js
@@ -1,29 +1,76 @@
-let id=787;
-let empId=String(id)
-// console.log(typeof(empId));
+// let id=787;
+// let empId=String(id)
+// // console.log(typeof(empId));
 
-// let eId=""//Type is number but value is 0
-// let eId="44jh"// Type is number but value is NaN
-// let eId=null //Type is number but value is 0
-// let eId=undefined //Type is number but value is NaN
-let eId=true //Type =number and value=1
-let eID=Number(eId);
-console.log(typeof(eID));
-console.log(eID);
+// // let eId=""//Type is number but value is 0
+// // let eId="44jh"// Type is number but value is NaN
+// // let eId=null //Type is number but value is 0
+// // let eId=undefined //Type is number but value is NaN
+// let eId=true //Type =number and value=1
+// let eID=Number(eId);
+// console.log(typeof(eID));
+// console.log(eID);
 
 
-let num=1 // true 
+// let num=1 // true 
 
-let num1=Boolean(num)
-console.log(num1);
+// let num1=Boolean(num)
+// console.log(num1);
 
-// let isAman=""// false
-// let isAman="aman" //true
+// // let isAman=""// false
+// // let isAman="aman" //true
 
-let isAman="278"
+// let isAman="278"
 
-let istrue=Boolean(isAman)
+// let istrue=Boolean(isAman)
 
-console.table([istrue,typeof istrue])
+// console.table([istrue,typeof istrue])
+
+
+// // String to Number cunversion
+// let na="aman"
+
+// let eName=Number(na)
+// console.table([na,eName,typeof eName])
+
+
+// ******************************OPERATIONS***************************************
+
+// let value=3
+// let negValue=-value
+// console.log(negValue);  output=-3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(11%2);
+// console.log(11/2);
+
+// let str1="Aman"
+// let str2=" Raj"
+// console.log(str1+str2);
+
+// console.log("1"+2);//12
+// console.log("1"+2+2);//122
+// console.log("1"+(2+2));//14
+// console.log(2+1+"2");//32
+
+// console.log(true);//true
+// console.log(+true);//1
+// console.log(-true);//-1
+
+let num1,num2,num3
+num1=num2=num3=3+9
+console.table([num1,num2,num3]);
+
+
+
+
+
+
+
+
 
 
