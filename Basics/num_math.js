dd3ebd812e6678fdc66aaf12a0764453d88f824f
Aman Raj <117666238+Amanraj0604@users.noreply.github.com>
2024-08-10 11:50:57 +0000

    updat code

---
+++ Basics/num_math.js
@@ -0,0 +1,36 @@
+const num=400.569
+const num1=new Number(100)
+
+// console.log(num1);
+
+const balence=num1.toString();
+// console.table([balence,typeof balence]);
+// console.table([num1.toFixed(1)]);//100.0
+
+const price=500000000
+// console.log(price.toLocaleString('en-IN'));//50,00,00,000
+
+
+//***********************MATHS*************************/
+
+// console.log(Math);//Object [Math] {}
+// console.log(Math.abs(-4));//4
+// console.log(Math.round(4.4));//4
+// console.log(Math.round(4.5));//5
+// console.log(Math.ceil(4.4));//5
+// console.log(Math.floor(4.4));//4
+
+
+console.log(Math.random());
+console.log(Math.round(Math.random()*6)+1);// output between 1 to 6 random
+
+
+let min=10;
+let max=20;
+
+console.log(Math.round(Math.random()*(max-min+1)+min));// output between 10 to 20
+
+
+
+
+
