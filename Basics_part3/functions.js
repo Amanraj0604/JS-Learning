6b7daa34b9b9e066b3c054e2b307fe4a3efa076e
Aman Raj <117666238+Amanraj0604@users.noreply.github.com>
2024-08-12 19:26:02 +0000

    update refrence link

---
+++ Basics_part3/functions.js
@@ -0,0 +1,29 @@
+// Syntax of function
+
+// function keword and work is the function name  
+function work(){
+    console.log("first function");
+}
+// work();// function calling
+
+
+//work :-function refrence
+//work() :- function calling
+
+
+//example 2
+
+function addTwoNumber(num1,num2){
+   // console.log(num1+num2);
+   return num1+num2
+    
+}
+
+addTwoNumber(8,9)//17
+
+const result=addTwoNumber(6,8)//14
+
+console.log(result)//undefined
+
+// Note :- when we return the added value from function then it assign the value in result variable and print the correct output
+
